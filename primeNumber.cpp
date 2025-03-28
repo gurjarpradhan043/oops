e3e264373011c10d2bb3aa936cc4b8653f286d57
Bhagwan Shahay <124359288+gurjarpradhan043@users.noreply.github.com>
2025-03-28 13:34:06 +0530

    Update primeNumber.cpp

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 bool isPrime(int num) {
     if(num < 2 ) return false;
-    for(int i=2; i<num/2; i++) {
+    for(int i=2; i<=sqrt/2; i++) {
         if(num % 2 == 0 ) return false;
 
     }
